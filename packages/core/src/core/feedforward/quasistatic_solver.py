d59716e78256048457a7a3178afcc3399dd69263
AdarWa <adar.wasserman@gmail.com>
2026-07-16 22:01:00 +0300

    Add docs to quasistatic solver

--- packages/core/src/core/feedforward/quasistatic_solver.py
+++ packages/core/src/core/feedforward/quasistatic_solver.py
@@ -8,6 +8,11 @@ from .gains import FeedforwardGains
 class QuasistaticSolver:
 
     def __init__(self) -> None:
+        """
+        Solver class for the quasistatic part of the motion.
+        Solves for K_s and K_v. The dynamic solver can then
+        be used after in order to find K_a based on the found K_s, K_v.
+        """
         self.problem = Problem()
         self.ks,self.kv = self.problem.decision_variable(2)
         self.problem.subject_to(self.ks >= 0)
@@ -16,6 +21,8 @@ class QuasistaticSolver:
     def init_data(self, data: list[LoggedSample]) -> None:
         J = 0
         for sample in data:
+            # The purpose of the quasistatic test is to find K_v and K_s, this can be easily done when the acceleration is zero.
+            # The quasistatic part must have an acceleration be as close as it can to zero.
             u_pred = eq.calculate_feedforward(self.ks, self.kv, 0, sample.velocity, 0)
             u = sample.voltage
             J += pow(u_pred-u, 2)
