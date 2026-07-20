3bc0f824d5bd53a61678895f798ba0e1bf4c0b80
AdarWa <adar.wasserman@gmail.com>
2026-07-20 17:23:24 +0300

    Starting cpp rewrite

--- packages/core/src/core/feedforward/eq.py
+++
@@ -1,9 +0,0 @@
-def signum(x):
-    if x > 0:
-        return 1
-    elif x < 0:
-        return -1
-    return 0
-
-def calculate_feedforward(ks, kv, ka, kg, v, a):
-    return ks * signum(v) + kv * v + ka * a + kg
