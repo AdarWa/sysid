e1b466580fec5f679cd8c2b93240eb55f0892b5a
AdarWa <adar.wasserman@gmail.com>
2026-07-21 14:59:09 +0300

    Make FeedforwardGains inherit from Gains struct

--- src/core/feedforward/gains.hpp
+++ src/core/feedforward/gains.hpp
@@ -5,15 +5,19 @@
 #ifndef SYSID_GAINS_HPP
 #define SYSID_GAINS_HPP
 
+#include "../optimization/OptimizationResult.hpp"
 #include "../sample/logged_sample.hpp"
 
 namespace sysid {
-    struct FeedforwardGains {
-        double ks;
-        double kv;
-        double ka;
-        double kg;
-        GravityType gravity;
+    struct FeedforwardGains : Gains{
+        double ks{0.0};
+        double kv{0.0};
+        double ka{0.0};
+        double kg{0.0};
+        GravityType gravity{GravityType::NONE};
+
+        FeedforwardGains() = default;
+        FeedforwardGains(const double ks, const double kv, const double ka, const double kg, const GravityType gravity) : ks(ks), kv(kv), ka(ka), kg(kg), gravity(gravity) {}
     };
 }
 
