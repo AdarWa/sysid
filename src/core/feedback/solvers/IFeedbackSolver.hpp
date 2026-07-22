8236c4ece9183b73d2cc82ab1ba8c95c0924b7be
AdarWa <adar.wasserman@gmail.com>
2026-07-22 12:27:02 +0300

    Add INPUTS to IFeedbackSolver interface

--- src/core/feedback/solvers/IFeedbackSolver.hpp
+++ src/core/feedback/solvers/IFeedbackSolver.hpp
@@ -9,18 +9,20 @@
 #include "../../sample/logged_sample.hpp"
 
 namespace sysid {
-    template <std::derived_from<Metrics> METRICS, std::derived_from<Gains> GAINS, typename TUNEABLES>
+    template <std::derived_from<Metrics> METRICS, std::derived_from<Gains> GAINS, typename TUNEABLES, typename INPUTS>
     class IFeedbackSolver {
         virtual ~IFeedbackSolver() = default;
 
         // Setters
         virtual void setSystem(const System& system) = 0;
         virtual void setTuneables(TUNEABLES tuneables) = 0;
+        virtual void setInputs(INPUTS inputs) = 0;
         virtual void setData(SampleVectorPtr data) = 0;
 
         // Getters
         virtual System& getSystem() = 0;
         virtual TUNEABLES getTuneables() = 0;
+        virtual INPUTS getInputs() = 0;
         virtual SampleVectorPtr getData() = 0;
 
         // Methods
