44b384a46fc0ef25da17c6fa1086babe53dd247b
AdarWa <adar.wasserman@gmail.com>
2026-07-24 19:34:29 +0300

    Add Ziegler-Nichols method for pid tuning

---
+++ src/core/feedback/solvers/fopdt/estimators/ZieglerNicholsEstimator.hpp
@@ -0,0 +1,21 @@
+//
+// Created by adarw on 7/24/26.
+//
+
+#ifndef SYSID_ZIEGLERNICHOLSESTIMATOR_HPP
+#define SYSID_ZIEGLERNICHOLSESTIMATOR_HPP
+#include "../IPIDEstimatorFOPDT.hpp"
+
+namespace sysid {
+    class ZieglerNicholsEstimator : IPIDEstimatorFOPDT {
+        PIDGains estimatePID(const FOPDTGains& gains) override {
+            return {
+                1.2 * (gains.tau / (gains.K * gains.theta)),
+                (0.6*gains.tau) / (gains.K * gains.theta * gains.theta),
+                0.6 * (gains.tau / gains.K)
+            };
+        }
+    };
+}
+
+#endif //SYSID_ZIEGLERNICHOLSESTIMATOR_HPP
